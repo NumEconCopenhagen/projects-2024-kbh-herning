1cf4d66fab086b0211fb3d8ac949d672b24e30d8
MarcusHonore <nxm852@alumni.ku.dk>
2024-03-20 15:23:15 +0100

    opgave 3 faerdig

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,4 +1,5 @@
 from types import SimpleNamespace
+import numpy as np
 
 class ExchangeEconomyClass:
 
@@ -13,6 +14,11 @@ class ExchangeEconomyClass:
         # b. endowments
         par.w1A = 0.8
         par.w2A = 0.3
+
+        # c. div
+        par.eps = 1e-8 #tolerance 
+        par.maxiter = 500 # iter
+        par.kappa = 0.1
 
     def utility_A(self,x1A,x2A):
         par = self.par
@@ -36,7 +42,7 @@ class ExchangeEconomyClass:
         par = self.par
         x1B = par.beta * ((p1*(1-par.w1A) + (1-par.w2A)) / p1)
         # x1B = [par.beta * ((price * (1-par.w1A) + (1-par.w2A)) / price) for price in p1]
-        x2B = (1-par.beta) * ((p1*par.w1A+par.w2A))
+        x2B = (1-par.beta) * ((p1*(1-par.w1A)+(1-par.w2A)))
         # x2B = [(1-par.beta) * ((price * (1-par.w1A) + (1-par.w2A))) for price in p1]
         return x1B, x2B
 
@@ -52,8 +58,101 @@ class ExchangeEconomyClass:
 
         return eps1,eps2
     
-    def solve(self):
-        # Indented block starts here
-        # Your code implementation here
-        pass  # Placeholder to indicate an empty method
-# No indentation beyond this point
+    def excess_demand_x1(self, p1):
+
+        par = self.par
+
+        x1A,x2A  = self.demand_A(p1)
+        x1B,x2B = self.demand_B(p1)
+
+        #a. demand
+        demand = x1A + x1B 
+
+        #b. supply
+        supply = 1 
+
+        #c. excess demand
+        excess_demand = demand - supply
+
+        return excess_demand
+    
+    def excess_demand_x2(self, p1):
+
+        x1A,x2A  = self.demand_A(p1)
+        x1B,x2B = self.demand_B(p1)
+
+        #a. demand
+        demand = x2A + x2B
+
+        #b. supply
+        supply = 1 
+
+        #c. excess demand
+        excess_demand = demand - supply
+
+        return excess_demand
+    
+    def find_equilibrium(self,p1_guess):
+        
+        par = self.par
+        
+        t=0
+        p1 = p1_guess
+        N = 2 # number of agents
+
+        # loop
+        while True:
+
+            # 1. excess_demand
+            z1 = self.excess_demand_x1(p1)
+
+            # 2. stop coomand
+            if np.abs(z1) < par.eps or t>=par.maxiter:
+                print(f'{t:3d}: p1={p1:12.8f} -> exess demand -> {z1:14.8f}')
+                break
+            
+            # 3. 
+            if t<5 or t%25==0:
+                print(f'{t:3d}: p1={p1:12.8f} -> exess demand -> {z1:14.8f}')
+
+            elif t==5:
+                print('     ...')
+
+            # 4. 
+            p1 = p1 + par.kappa*z1/N
+
+            # 5. 
+            t += 1 
+
+        # Checjk if solution is found 
+        if np.abs(z1) < par.eps:
+            
+            # store eguilibrium
+            par.p1_star = p1 
+
+            par.z1 = z1
+            par.z2 = self.excess_demand_x2(par.p1_star)
+
+            if not np.abs(par.z2) < par.eps: 
+                print('the market for good 2 was not cleared')
+                print(f'z2={par.z2}')
+            
+        else:
+            print('solution was not found')
+    
+    def print_solution(self):
+
+        par = self.par
+
+        text = 'solution to market equilibrium:\n'
+        text += f'p1 = {par.p1_star:5.3f}\n'
+
+        text += 'excess demand ar:\n'
+        text += f'z1 = {par.z1}\n'
+        text += f'z2= {par.z2}'
+        print(text)
+
+
+
+
+
